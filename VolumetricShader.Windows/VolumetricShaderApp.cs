27c2d4b315e045e5258bfc75be80a2ad056d65b2
KAFIA Youness <kafia.youness@hotmail.fr>
2020-11-12 21:27:43 +0100

    first commit

---
+++ VolumetricShader.Windows/VolumetricShaderApp.cs
@@ -0,0 +1,15 @@
+using Stride.Engine;
+
+namespace VolumetricShader
+{
+    class VolumetricShaderApp
+    {
+        static void Main(string[] args)
+        {
+            using (var game = new Game())
+            {
+                game.Run();
+            }
+        }
+    }
+}
---
+++ VolumetricShader/UpdateCameraPos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stride.Core.Mathematics;
+using Stride.Input;
+using Stride.Engine;
+using VolumetricShader.Effects;
+
+namespace VolumetricShader
+{
+    public class UpdateCameraPos : SyncScript
+    {
+        // Declared public member fields and properties will show in the game studio
+        public Entity camera;
+
+
+        public override void Start()
+        {
+            // Initialization of the script.
+        }
+
+        public override void Update()
+        {
+            //var materials = Entity.Get<ModelComponent>().Materials;
+            var material = Entity.Get<ModelComponent>().Materials.First().Value;
+            material.Passes.First().Parameters.Set(VolumetricSphereKeys.CameraPos, camera.Transform.Position);
+        }
+    }
+}
